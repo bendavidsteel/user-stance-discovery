275fec476512d3d1d8362ae8ae043360360cc4e1
Ben Steel <bendavidsteel@gmail.com>
2023-07-03 10:18:38 +0000

    Working on markov chain

--- scripts/model_choices.py
+++ scripts/model_choices.py
@@ -7,13 +7,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import pyro
-import pyro.distributions as dist
 import pyro.distributions.constraints as constraints
+from pyro.generic import distributions as dist
+from pyro.generic import infer, ops, optim, pyro, pyro_backend
 
 
 class InteractionDataset(torch.utils.data.Dataset):
-    def __init__(self, data_path):
+    def __init__(self, data_path, batch=True):
+        self.batch = batch
+
         comments_df = pd.read_csv(os.path.join(data_path, 'sample_comments.csv'), dtype={'author_name': str, 'author_id': str, 'comment_id': str, 'video_id': str, 'reply_comment_id': str})
         # fix annoying reply_comment_id stored as exponent format
         comments_df = comments_df.loc[:, ~comments_df.columns.str.contains('^Unnamed')]
@@ -37,44 +39,80 @@ class InteractionDataset(torch.utils.data.Dataset):
         self.videos_df = videos_df.reset_index()
 
         self.videos_comments_df = self.comments_df.sort_values(['video_id', 'createtime']).groupby('video_id')
-        self.max_base_comments = self.comments_df[self.comments_df['reply_comment_id'].isna()][['video_id', 'comment_id']].groupby('video_id').count().max().values[0]
+        self.user_comments_df = self.comments_df.sort_values(['author_id', 'createtime']).groupby('author_id')
+
+        self.users_df = self.comments_df[['author_id', 'author_name']].drop_duplicates().reset_index()
 
     def __len__(self):
-        return len(self.comments_df)
+        return len(self.users_df)
 
     def __getitem__(self, idx):
-        comment = self.comments_df.iloc[idx]
-        video_comments = self.videos_comments_df.get_group(comment['video_id'])
+        users = self.users_df.iloc[idx]
+        comments = pd.concat([self.user_comments_df.get_group(user_id) for user_id in users['author_id']])
+        video_ids = comments['video_id'].unique()
+        video_comments = pd.concat([self.videos_comments_df.get_group(video_id) for video_id in video_ids])
         # get only level 1 comments
         base_comments = video_comments[video_comments['reply_comment_id'].isna()]
         comment_opinions = self.comments_topic_probs[base_comments.index.values]
 
-        # add on 0s vec as null comment
-        padded_comment_opinions = np.zeros((self.max_base_comments + 1, comment_opinions.shape[1]))
-        padded_comment_opinions[1:comment_opinions.shape[0]+1,:] = comment_opinions
-        mask_comment_opinions = np.zeros((self.max_base_comments + 1,))
-        mask_comment_opinions[:comment_opinions.shape[0]+1] = 1
+        if self.batch:
+            num_users = len(users)
+            max_comment_seq = comments[['author_id', 'comment_id']].groupby('author_id').count().max().values[0]
+            max_base_comments = base_comments[['video_id', 'comment_id']].groupby('video_id').count().max().values[0]
+            # add on 0s vec as null comment
+            padded_comment_opinions = np.zeros((num_users, max_comment_seq, max_base_comments + 1, comment_opinions.shape[1]))
+            mask_comment_opinions = np.zeros((num_users, max_comment_seq, max_base_comments + 1,))
+            reply_comment = np.zeros((num_users, max_comment_seq), dtype=np.int64)
+            actual_comment = np.zeros((num_users, max_comment_seq, comment_opinions.shape[1]))
 
-        if not isinstance(comment['reply_comment_id'], str) and np.isnan(comment['reply_comment_id']):
-            reply_comment_idx = 0
+            for i, user_id in enumerate(users['author_id']):
+                comments = self.user_comments_df.get_group(user_id)
+                for j, (comment_idx, comment) in enumerate(comments.iterrows()):
+                    # get all comments for video
+                    video_comments = self.videos_comments_df.get_group(comment['video_id'])
+                    base_comments = video_comments[video_comments['reply_comment_id'].isna()]
+                    comment_opinions = self.comments_topic_probs[base_comments.index.values]
+
+                    # fill in the comment opinions
+                    padded_comment_opinions[i, j, 1:comment_opinions.shape[0]+1, :] = comment_opinions
+                    mask_comment_opinions[i, j, :comment_opinions.shape[0]+1] = 1
+
+                    # fill in the reply comment
+                    if not isinstance(comment['reply_comment_id'], str) and np.isnan(comment['reply_comment_id']):
+                        reply_comment_idx = 0
+                    else:
+                        reply_comments = video_comments[video_comments['comment_id'] == comment['reply_comment_id']]
+                        assert len(reply_comments) == 1
+                        all_comments_reply_idx = reply_comments.index.values[0]
+                        # adding 1 because we append the null comment zero vector at the start
+                        reply_comment_idx = np.where(base_comments.index.values == all_comments_reply_idx)[0][0] + 1
+
+                    reply_comment[i, j] = reply_comment_idx
+
+                    # fill in the actual comment
+                    actual_comment[i, j, :] = self.comments_topic_probs[comment_idx]
         else:
-            reply_comments = video_comments[video_comments['comment_id'] == comment['reply_comment_id']]
-            assert len(reply_comments) == 1
-            all_comments_reply_idx = reply_comments.index.values[0]
-            # adding 1 because we append the null comment zero vector at the start
-            reply_comment_idx = np.where(base_comments.index.values == all_comments_reply_idx)[0][0] + 1
+            raise NotImplementedError()
+            padded_comment_opinions = np.zeros((len(base_comments) + 1, comment_opinions.shape[1]))
+            padded_comment_opinions[1:comment_opinions.shape[0]+1,:] = comment_opinions
+            mask_comment_opinions = np.ones((len(base_comments) + 1,))
+
         return {
-            'user_state': torch.zeros(self.video_topic_probs.shape[1]),
             'padded_comments_opinions': torch.tensor(padded_comment_opinions),
             'mask_comments_opinions': torch.tensor(mask_comment_opinions),
-            'reply_comment': torch.tensor(reply_comment_idx)
+            'reply_comment': torch.tensor(reply_comment),
+            'actual_comment': torch.tensor(actual_comment),
         }
+
 
 def uniform_probs(num):
     return torch.full((num,), 1 / num)
 
-def uniform_probs_from_mask(mask):
-    return mask / torch.sum(mask, dim=1, keepdim=True)
+def uniform_probs_from_mask(mat_mask):
+    vec_mask = mat_mask.any(dim=1)
+    uniform_probs = torch.zeros(mat_mask.shape, dtype=torch.float64)
+    uniform_probs[vec_mask] = mat_mask[vec_mask] / torch.sum(mat_mask, dim=1, keepdim=True)[vec_mask]
+    return uniform_probs
 
 def degroot(user_state, new_content, attention):
     return user_state + torch.bmm(attention.unsqueeze(1), new_content).squeeze(1)
@@ -92,8 +130,15 @@ def stochastic_bounded_confidence_categorical(user_state, new_content, content_m
     unsq_content_mask = content_mask.unsqueeze(2)
     # we can't use cdist because we can't mask in cdist, and unmasked cdist would be computationally inefficient
     content_dist = torch.cdist(user_state.unsqueeze(1), new_content * unsq_content_mask).squeeze(1) * content_mask
-    numerator = torch.pow(content_dist, -1 * exponent.unsqueeze(1)) * content_mask
-    return numerator / torch.sum(numerator, dim=1, keepdims=True)
+
+    vec_mask = content_mask.any(dim=1)
+
+    # TODO use MaskedTensor
+    numerator = torch.pow(content_dist + ((1 - content_mask) * 1), -1 * exponent.unsqueeze(1)) * content_mask
+    probs = torch.zeros(numerator.shape, dtype=torch.float64)
+    probs[vec_mask] = numerator[vec_mask] / torch.sum(numerator, dim=1, keepdims=True)[vec_mask]
+
+    return probs
 
 def stochastic_bounded_confidence_bernoulli(user_state, new_content, exponent, n, avg_content):
     # compares prob of interaction between a number of people
@@ -133,6 +178,8 @@ def write_comment_opinions(user_state):
     return user_state
 
 def recommend_videos(available_videos, user_history, actual_video=None):
+    # TODO any platform parameters must be sampled here, outside the plate
+    # TODO this has implications for how we can subsample
     # tiktok recommends a video
     with pyro.plate("actions", num_data):
         video_probs = recommend_potential_videos(available_videos, user_history)
@@ -161,25 +208,64 @@ def choose_video(recommended_video, user_state, avg_video, actual_chosen_video=N
         chosen_video = pyro.sample("choose_video", dist.Bernoulli(choose_video_probs), obs=actual_chosen_video)
         return chosen_video, user_state
 
-def choose_reply_comment(user_state, padded_comments_opinions, mask_comment_opinions, actual_reply_comment=None):
-    with pyro.plate("actions", len(padded_comments_opinions)):
-        # comments are then shown on video and causes state update
+def choose_reply_comment(t, user_state, padded_comments_opinions, mask_comment_opinions, actual_reply_comment=None):
+    mask = mask_comment_opinions.any(axis=1)
+    with pyro.poutine.mask(mask=mask):
         comment_attention = uniform_probs_from_mask(mask_comment_opinions)
         user_state = state_update(user_state, padded_comments_opinions, comment_attention)
 
         # select a comment to reply to
         # TODO maybe pick different prior
-        comment_diff_weight = pyro.sample("comment_diff_weight", dist.Normal(0, 1))
+        comment_diff_weight_loc = pyro.param("comment_diff_weight_loc", init_tensor=torch.tensor(0.))
+        comment_diff_weight_scale = pyro.param("comment_diff_weight_scale", init_tensor=torch.tensor(1.), constraint=constraints.positive)
+        comment_diff_weight = pyro.sample(f"comment_diff_weight_{t}", dist.Normal(comment_diff_weight_loc, comment_diff_weight_scale))
+
+        assert not comment_diff_weight.isnan().all(), "comment_diff_weight has nan values"
         comment_probs = choose_comment(padded_comments_opinions, mask_comment_opinions, user_state, comment_diff_weight)
-        chosen_comment = pyro.sample("choose_comment", dist.Categorical(probs=comment_probs), obs=actual_reply_comment)
+
+        assert torch.logical_or(comment_probs[np.arange(comment_probs.shape[0]), actual_reply_comment] > 0, ~mask).all(), "Actual reply comment has 0 probability"
+        # ensure simplex criteria is met for masked values
+        comment_probs[~mask, 0] = 1
+        chosen_comment = pyro.sample(f"choose_comment_{t}", dist.Categorical(probs=comment_probs).mask(mask), obs=actual_reply_comment)
         return chosen_comment, user_state
 
-def write_comment(user_state, actual_comment=None):
-    with pyro.plate("actions", len(user_state)):
-        # write a comment
-        comment_opinions = write_comment_opinions(user_state)
-        our_comment = pyro.sample("our_comment", dist.Normal(loc=comment_opinions, scale=1), obs=actual_comment)
-        return our_comment
+def choose_reply_comment_guide(user_state, padded_comments_opinions, mask_comment_opinions, actual_reply_comment=None):
+    with pyro.plate("choose_actions", len(padded_comments_opinions), dim=-1):
+        for t in pyro.markov(range(padded_comments_opinions.shape[1])):
+            # comments are then shown on video and causes state update
+            # comment_attention = uniform_probs_from_mask(mask_comment_opinions)
+            # user_state = state_update(user_state, padded_comments_opinions, comment_attention)
+
+            # select a comment to reply to
+            # TODO maybe pick different prior
+            comment_diff_weight_loc = pyro.param("comment_diff_weight_loc_q", init_tensor=torch.tensor(0.), event_dim=1)
+            pyro.sample(f"comment_diff_weight_{t}", dist.Delta(comment_diff_weight_loc))
+
+def write_comment(t, user_state, actual_comment=None):
+    # with pyro.plate("write_actions", user_state.shape[0], dim=-2):
+    # write a comment
+    comment_opinions = write_comment_opinions(user_state)
+    comment_scale = pyro.param("comment_scale", torch.tensor(1.), constraint=constraints.positive)
+    comment_scale = comment_scale.expand(comment_opinions.shape)
+    comment_cov = torch.zeros((*comment_opinions.shape, 1), dtype=torch.float64)
+    our_comment = pyro.sample(f"our_comment_{t}", dist.LowRankMultivariateNormal(comment_opinions, comment_cov, comment_scale), obs=actual_comment)
+    return our_comment
+
+def reply_and_write_comment(padded_comments_opinions, mask_comment_opinions, actual_reply_comment=None, actual_comment=None):
+    with pyro.plate("actions", padded_comments_opinions.shape[0]):
+        user_state = torch.zeros((padded_comments_opinions.shape[0], padded_comments_opinions.shape[-1]))
+        for t in pyro.markov(range(padded_comments_opinions.shape[1])):
+            t_padded_comments_opinions = padded_comments_opinions[:, t]
+            t_mask_comment_opinions = mask_comment_opinions[:, t]
+            t_actual_reply_comment = actual_reply_comment[:, t]
+            t_actual_comment = actual_comment[:, t]
+
+            reply_comment, user_state = choose_reply_comment(t, user_state, t_padded_comments_opinions, t_mask_comment_opinions, actual_reply_comment=t_actual_reply_comment)
+            our_comment = write_comment(t, user_state, actual_comment=t_actual_comment)
+    return reply_comment, our_comment, user_state
+
+def reply_and_write_comment_guide(padded_comments_opinions, mask_comment_opinions, actual_reply_comment=None, actual_comment=None):
+    choose_reply_comment_guide(padded_comments_opinions, mask_comment_opinions, actual_reply_comment)
 
 def model(available_videos, user_history, actual_video=None, actual_chosen_video=None, actual_replied_comment=None, actual_comment=None):
 
@@ -187,6 +273,7 @@ def model(available_videos, user_history, actual_video=None, actual_chosen_video
     num_data = len(user_history)
     
     # TODO use user previous actions
+    # TODO this is a markov process, use pyro.markov
     # if not user_state:
     user_state = pyro.sample("initial_user_state", dist.MultivariateNormal())
     recommended_video = recommend_videos(available_videos, user_history, video=actual_video)
@@ -200,58 +287,78 @@ def model(available_videos, user_history, actual_video=None, actual_chosen_video
 
         return recommended_video, chosen_video, reply_comment, comment
 
-def get_trainer(model_func):
-    auto_guide = pyro.infer.autoguide.AutoNormal(model_func)
-    adam = pyro.optim.Adam({"lr": 0.02})
-    elbo = pyro.infer.Trace_ELBO()
-    svi = pyro.infer.SVI(model_func, auto_guide, adam, elbo)
+def get_trainer(model_func, guide=None):
+    if guide is None:
+        guide = infer.autoguide.AutoDelta(model_func)
+    adam = optim.ClippedAdam({"lr": 0.001})
+    elbo = infer.Trace_ELBO()
+    svi = infer.SVI(model_func, guide, adam, elbo)
     return svi
+
+def training(dataloader, batch_size):
+
+    backend = "pyro"
+    with pyro_backend(backend):
+
+        if backend == "pyro":
+            pyro.enable_validation(True)
+            pyro.set_rng_seed(1)
+
+        trainer = get_trainer(reply_and_write_comment)#, guide=reply_and_write_comment_guide)
+
+        num_epochs = 100
+
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            num_preds = 0
+
+            # Iterate over data.
+            bar = tqdm.tqdm(
+                dataloader,
+                desc="Epoch {}".format(epoch),
+            )
+            for i, batch in enumerate(bar):
+                padded_comments_opinions = batch['padded_comments_opinions']
+                mask_comments_opinions = batch['mask_comments_opinions']
+                reply_comment = batch['reply_comment']
+                actual_comment = batch['actual_comment']
+
+                loss = trainer.step(padded_comments_opinions, mask_comments_opinions, actual_reply_comment=reply_comment, actual_comment=actual_comment)
+
+                # statistics
+                running_loss += loss / batch_size
+                num_preds += 1
+                if i % 10 == 0:
+                    bar.set_postfix(
+                        loss="{:.2f}".format(running_loss / num_preds),
+                    )
+
+            epoch_loss = running_loss / len(dataloader.dataset)
+
+        alpha_q = pyro.param("alpha_q").item()
+        beta_q = pyro.param("beta_q").item()
 
 def main():
     this_dir_path = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(this_dir_path, '..', 'data', 'bertweet_base_5_100_100')
 
-    pyro.enable_validation(True)
-    pyro.set_rng_seed(1)
+    batch_size = 64
+    num_workers = 0
+    pin_memory = False
+    shuffle = False
 
-    inter_dataset = InteractionDataset(data_path)
-    inter_dataloader = torch.utils.data.DataLoader(inter_dataset, batch_size=64, shuffle=True) #, num_workers=4)
+    dataset = InteractionDataset(data_path)
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        sampler=torch.utils.data.BatchSampler(
+            torch.utils.data.SequentialSampler(dataset), batch_size=batch_size, drop_last=False
+        ),
+        batch_size=None,
+        num_workers=num_workers, 
+        pin_memory=pin_memory
+    )
 
-    trainer = get_trainer(choose_reply_comment)
-
-    num_epochs = 100
-
-    for epoch in range(num_epochs):
-        running_loss = 0.0
-        num_preds = 0
-
-        # Iterate over data.
-        bar = tqdm.tqdm(
-            inter_dataloader,
-            desc="Epoch {}".format(epoch),
-        )
-        for i, batch in enumerate(bar):
-            user_state = batch['user_state']
-            padded_comments_opinions = batch['padded_comments_opinions']
-            mask_comments_opinions = batch['mask_comments_opinions']
-            reply_comment = batch['reply_comment']
-
-            loss = trainer.step(user_state, padded_comments_opinions, mask_comments_opinions, actual_reply_comment=reply_comment)
-
-            # statistics
-            running_loss += loss / inputs.size(0)
-            num_preds += 1
-            if i % 10 == 0:
-                bar.set_postfix(
-                    loss="{:.2f}".format(running_loss / num_preds),
-                )
-
-        epoch_loss = running_loss / len(inter_dataset)
-
-    plt.figure(figsize=(5, 2))
-    plt.plot(losses)
-    plt.xlabel("SVI step")
-    plt.ylabel("ELBO loss")
+    training(dataloader, batch_size)
 
 if __name__ == '__main__':
     main()
---
+++ scripts/test_choices_examples.py
@@ -0,0 +1,47 @@
+import os
+
+import torch
+
+import pyro
+
+import model_choices
+
+def test_choices_examples():
+    this_dir_path = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(this_dir_path, '..', 'data', 'bertweet_base_5_100_100')
+
+    batch_size = 1
+    num_workers = 0
+    pin_memory = False
+    shuffle = True
+
+    inter_dataset = model_choices.InteractionDataset(data_path)
+    inter_dataloader = torch.utils.data.DataLoader(inter_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
+
+    pyro.enable_validation(True)
+    pyro.set_rng_seed(1)
+
+    trainer = model_choices.get_trainer(model_choices.choose_reply_comment)#, guide=choose_reply_comment_guide)
+
+    example_seqs = [[0, 1]]
+
+    # 14103
+    # 10244
+    # 5245
+
+    for example_seq in example_seqs:
+        running_loss = 0.
+        for example_idx in example_seq:
+            example = inter_dataset[example_idx]
+            batch = inter_dataloader.collate_fn([example])
+
+            user_state = batch['user_state']
+            padded_comments_opinions = batch['padded_comments_opinions']
+            mask_comments_opinions = batch['mask_comments_opinions']
+            reply_comment = batch['reply_comment']
+
+            loss = trainer.step(user_state, padded_comments_opinions, mask_comments_opinions, actual_reply_comment=reply_comment)
+            running_loss += loss / batch_size
+
+if __name__ == "__main__":
+    test_choices_examples()
