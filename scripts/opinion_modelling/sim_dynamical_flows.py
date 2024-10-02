415dd25324fa2b6a427b41eea59c8e0ee77d3714
Ben Steel <bendavidsteel@gmail.com>
2024-10-02 17:30:58 -0400

    Achieved GP with ordinal likelihood

--- scripts/opinion_modelling/sim_dynamical_flows.py
+++ scripts/opinion_modelling/sim_dynamical_flows.py
@@ -1,3 +1,4 @@
+import datetime
 
 import gpytorch
 from matplotlib.animation import FuncAnimation
@@ -15,8 +16,8 @@ from mining.generative import SocialGenerativeModel, UpdateDist
 
 def main():
     num_steps = 100
-    num_users = 50
-    num_opinions = 3
+    num_users = 10
+    num_opinions = 8
     social_context = SocialGenerativeModel(
         num_users=num_users, 
         sbc_exponent_loc=1.0,
@@ -40,7 +41,13 @@ def main():
 
     X_norm, y = prep_gp_data(dataset)
     model_list, likelihood_list, losses = train_gaussian_process(X_norm, y)
-    timestamps, means = get_gp_means(dataset, model_list, likelihood_list, X_norm)
+    timestamps, means = get_gp_means(dataset, model_list, likelihood_list, X_norm, y)
+
+    min_time = datetime.datetime(2023, 1, 1, 0, 0, 0)
+    max_time = datetime.datetime(2023, 12, 31, 23, 59, 59)
+
+    unix_timestamps = min_time.timestamp() + (max_time.timestamp() - min_time.timestamp()) * timestamps / np.max(timestamps)
+    datetimes = np.array([datetime.datetime.fromtimestamp(t) for t in unix_timestamps])
 
     fig, ax = plt.subplots(figsize=(12, 6))
     ax.plot(losses)
@@ -48,10 +55,11 @@ def main():
     ax.set_ylabel("Loss")  # supress output text
     fig.savefig(f'./figs/flows/losses.png')
 
-    fig, axes = flow_pairplot(timestamps, means)
+    figs, axes = flow_pairplot(datetimes, means, dataset.stance_columns, do_pairplot=False)
 
-    fig.tight_layout()
-    fig.savefig('./figs/flows/flows.png')
+    for i in range(num_opinions):
+        for j in range(num_opinions):
+            figs[i,j].savefig(f'./figs/flows/{i}_{j}_flows.png')
 
             
 if __name__ == '__main__':
