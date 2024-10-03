c68341dbe362131d7029b838f5a58f352fd31677
Ben Steel <bendavidsteel@gmail.com>
2024-10-03 17:59:01 -0400

    Switching to try splines

--- scripts/opinion_modelling/test_curve_fitters.py
+++ scripts/opinion_modelling/test_curve_fitters.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 
 from mining import estimate, datasets
-from mining.estimate import prep_gp_data, train_gaussian_process, get_gp_means, get_gp_models
+from mining.estimate import prep_gp_data, train_gaussian_process, get_gp_means, get_gp_models, get_splines
 
 def get_expected_class(model_output):
     # Convert to probabilities using exp
@@ -17,7 +17,7 @@ def get_expected_class(model_output):
     expected_mean = expected_class - 1
     return expected_mean
 
-def plot(
+def plot_gp(
     model,
     likelihood,
     train_x=None,
@@ -46,7 +46,7 @@ def plot(
             end_x = max_x + 0.1 * (max_x - min_x)
             test_x = torch.linspace(start_x, end_x, n_test).to('cuda')
             test_classifier_ids = torch.zeros(n_test, dtype=torch.long)
-            model_pred = model.predict(test_x)
+            model_pred = model(test_x)
             # likelihood.set_classifier_ids(test_classifier_ids)
             # observed_pred = likelihood(model_pred)
             # Get upper and lower confidence bounds
@@ -61,7 +61,36 @@ def plot(
         ax.set_ylim([-3, 3])
         ax.legend(['Observed Data', 'Mean', 'Confidence'])
 
+def plot_spline(
+    model,
+    train_x=None,
+    train_y=None,
+    plot_observed_data=False,
+    plot_predictions=False,
+    n_prior_samples=0,
+    n_test=10,
+    ax=None,
+):
 
+    # Initialize plot
+    if ax is None:
+        f, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+    if plot_observed_data:
+        # Plot training data as black stars
+        ax.plot(train_x.numpy(), train_y.numpy(), 'k*')
+    if plot_predictions:
+        min_x = torch.min(train_x)
+        max_x = torch.max(train_x)
+        start_x = min_x - 0.1 * (max_x - min_x)
+        end_x = max_x + 0.1 * (max_x - min_x)
+        test_x = torch.linspace(start_x, end_x, n_test)
+        test_y = model.predict(test_x.reshape(-1, 1)).reshape(-1)  
+        # Plot predictive means as blue line
+        # mean = get_expected_class(model_pred.loc.T)
+        ax.plot(test_x.numpy(), test_y, 'b')
+    ax.set_ylim([-3, 3])
+    ax.legend(['Observed Data', 'Mean', 'Confidence'])
 
 def main():
     num_data_points = 10
@@ -77,42 +106,51 @@ def main():
     lengthscale_loc = time_span_ratio * 0.01
     lengthscale_scale = time_span_ratio * 0.1
 
-    model_list, likelihood_list, model_map, train_xs, train_ys = get_gp_models(
-        X_norm, 
-        y, 
-        classifier_ids, 
-        all_classifier_profiles, 
-        lengthscale_loc=lengthscale_loc, 
-        lengthscale_scale=lengthscale_scale,
-        gp_type='ordinal'
-    )
+    model_type = 'spline'
+    if model_type == 'gp':
+        model_list, likelihood_list, model_map, train_xs, train_ys = get_gp_models(
+            X_norm, 
+            y, 
+            classifier_ids, 
+            all_classifier_profiles, 
+            lengthscale_loc=lengthscale_loc, 
+            lengthscale_scale=lengthscale_scale,
+            gp_type='ordinal'
+        )
 
-    # get some samples from the untrained models
-    # fig, ax = plt.subplots(1, 1, figsize=(4, 3))
-    # for idx in range(min(3, len(model_list.models))):
-    #     i, k = model_map[idx]
-    #     model = model_list.models[idx]
-    #     likelihood = likelihood_list.likelihoods[idx]
-    #     plot(model, likelihood, train_x=X_norm[i,:], train_y=y[i,:,k], plot_observed_data=True, plot_predictions=True, ax=ax)
-    # plt.show()
-    
-    model_list, likelihood_list, model_map, losses = train_gaussian_process(
-        X_norm, 
-        y, 
-        classifier_ids, 
-        all_classifier_profiles, 
-        lengthscale_loc=lengthscale_loc, 
-        lengthscale_scale=lengthscale_scale,
-        gp_type='ordinal',
-    )
+        # get some samples from the untrained models
+        # fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+        # for idx in range(min(3, len(model_list.models))):
+        #     i, k = model_map[idx]
+        #     model = model_list.models[idx]
+        #     likelihood = likelihood_list.likelihoods[idx]
+        #     plot(model, likelihood, train_x=X_norm[i,:], train_y=y[i,:,k], plot_observed_data=True, plot_predictions=True, ax=ax)
+        # plt.show()
+        
+        model_list, likelihood_list, model_map, losses = train_gaussian_process(
+            X_norm, 
+            y, 
+            classifier_ids, 
+            all_classifier_profiles, 
+            lengthscale_loc=lengthscale_loc, 
+            lengthscale_scale=lengthscale_scale,
+            gp_type='ordinal',
+        )
+    elif model_type == 'spline':
+        model_list, model_map = get_splines(X_norm, y)
 
     # get some samples from the untrained models
     fig, ax = plt.subplots(1, 1, figsize=(4, 3))
     for idx in range(min(3, len(model_list))):
-        i, k = model_map[idx]
-        model = model_list[idx]
-        likelihood = likelihood_list[idx]
-        plot(model, likelihood, train_x=X_norm[i,:], train_y=y[i,:,k], plot_observed_data=True, plot_predictions=True, ax=ax)
+        if model_type == 'gp':
+            i, k = model_map[idx]
+            model = model_list[idx]
+            likelihood = likelihood_list[idx]
+            plot_gp(model, likelihood, train_x=X_norm[i,:], train_y=y[i,:,k], plot_observed_data=True, plot_predictions=True, ax=ax)
+        elif model_type == 'spline':
+            i, k = model_map[idx]
+            model = model_list[idx]
+            plot_spline(model, train_x=X_norm[i,:], train_y=y[i,:,k], plot_observed_data=True, plot_predictions=True, ax=ax)
     plt.show()
 
     # timestamps, means = get_gp_means(dataset, model_list, likelihood_list, model_map, X_norm, X, y)
