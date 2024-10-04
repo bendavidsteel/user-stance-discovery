ed3f960f3c10c088aa05a2e9849c342b88b3d708
Ben Steel <bendavidsteel@gmail.com>
2024-10-03 22:32:28 -0400

    Working on flow fields

--- scripts/opinion_modelling/reddit_flow_fields.py
+++ scripts/opinion_modelling/reddit_flow_fields.py
@@ -7,11 +7,7 @@ import numpy as np
 import torch
 import tqdm
 
-from mining.datasets import RedditOpinionTimelineDataset
 from mining.dynamics import flow_pairplot
-from mining.estimate import prep_gp_data, train_gaussian_process, get_gp_means, get_splines, get_spline_means
-
-from test_curve_fitters import plot_spline
 
 def main():
     this_dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -29,22 +25,26 @@ def main():
     model_type = 'spline'
 
     stance_columns = ['vaccine_mandates', 'renter_protections', 'ndp', 'liberals', 'conservatives', 'gun_control', 'drug_decriminalization', 'liberal_immigration_policy', 'canadian_aid_to_ukraine']
-    timestamps = np.load(os.path.join(root_dir_path, "data", data_source, source_dir, "timestamps.npy"))
-    means = np.load(os.path.join(root_dir_path, "data", data_source, source_dir, "means.npy"))
-    if model_type == 'gp':
-        confidence_region = np.load(os.path.join(root_dir_path, "data", data_source, source_dir, "confidence_region.npy"))
+    stance_names = ['Vaccine Mandates', 'Renter Protections', 'NDP', 'Liberals', 'Conservatives', 'Gun Control', 'Drug Decriminalization', 'Liberal Immigration Policy', 'Canadian Aid to Ukraine']
+    timestamps = np.load(os.path.join(root_dir_path, "data", data_source, source_dir, model_type, "timestamps.npy"))
+    means = np.load(os.path.join(root_dir_path, "data", data_source, source_dir, model_type, "means.npy"))
+    confidence_intervals = np.load(os.path.join(root_dir_path, "data", data_source, source_dir, model_type, "confidence_region.npy"))
 
 
-    flow_dir_path = os.path.join(root_dir_path, "figs", data_source, source_dir, "flows")
+    flow_dir_path = os.path.join(root_dir_path, "figs", data_source, source_dir, model_type, "flows")
     os.makedirs(flow_dir_path, exist_ok=True)
 
     # TODO ensure timestamps are correctly plotted, the figure has suspiciously many people with similar start times
-    figs, axes = flow_pairplot(timestamps, means, stance_columns, do_pairplot=False)
+    figs, axes, flow_figs, flow_axes = flow_pairplot(timestamps, means, confidence_intervals, stance_names, do_pairplot=False, save=True, save_path=flow_dir_path)
 
-    for i in range(len(axes)):
-        for j in range(len(axes[i])):
-            fig = figs[i, j]
-            fig.savefig(os.path.join(flow_dir_path, f'{i}_{j}_flows.png'))
+    # for i in range(len(axes)):
+    #     for j in range(len(axes[i])):
+    #         fig = figs[i, j]
+    #         fig.savefig(os.path.join(flow_dir_path, f'{i}_{j}_flows.png'))
+
+    # for i in range(len(flow_axes)):
+    #     fig = flow_figs[i]
+    #     fig.savefig(os.path.join(flow_dir_path, f'{i}_flows.png'))
 
             
 if __name__ == '__main__':
--- scripts/opinion_modelling/reddit_stance_timelines.py
+++ scripts/opinion_modelling/reddit_stance_timelines.py
@@ -1,4 +1,4 @@
-
+import datetime
 import os
 
 import gpytorch
@@ -11,7 +11,42 @@ from mining.datasets import RedditOpinionTimelineDataset
 from mining.dynamics import flow_pairplot
 from mining.estimate import prep_gp_data, train_gaussian_process, get_gp_means, get_splines, get_spline_means
 
-from test_curve_fitters import plot_spline
+def plot_spline(
+    timeline, mean, confidence_interval,
+    train_x=None,
+    train_y=None,
+    plot_observed_data=False,
+    plot_predictions=False,
+    plot_legend=False,
+    n_test=100,
+    ax=None,
+    opinion_name=None
+):
+
+    # Initialize plot
+    if ax is None:
+        f, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+    if plot_observed_data:
+        # Plot training data as black stars
+        ax.plot([datetime.datetime.fromtimestamp(int(t)) for t in train_x.numpy()], train_y.numpy(), 'k*')
+    if plot_predictions:
+        # min_x = torch.min(train_x)
+        # max_x = torch.max(train_x)
+        # start_x = min_x - 0.1 * (max_x - min_x)
+        # end_x = max_x + 0.1 * (max_x - min_x)
+        # test_x = torch.linspace(start_x, end_x, n_test)
+        # test_y = model.predict(test_x.reshape(-1, 1)).reshape(-1)  
+        # Plot predictive means as blue line
+        # mean = get_expected_class(model_pred.loc.T)
+        # ax.plot(test_x.numpy(), test_y, 'b')
+        ax.plot([datetime.datetime.fromtimestamp(int(t)) for t in timeline], mean, 'b')
+        ax.fill_between([datetime.datetime.fromtimestamp(int(t)) for t in timeline], confidence_interval[:,0], confidence_interval[:,1], alpha=0.5)
+    ax.set_ylim([-1.5, 1.5])
+    if plot_legend:
+        ax.legend(['Observed Data', 'Mean', 'Confidence'])
+    
+    ax.set_title(opinion_name)
 
 def plot(
     model,
@@ -20,9 +55,11 @@ def plot(
     train_y=None,
     plot_observed_data=False,
     plot_predictions=False,
+    plot_legend=False,
     n_prior_samples=0,
     n_test=500,
     ax=None,
+    opinion_name=None
 ):
     # Get into evaluation (predictive posterior) mode
     model.eval()
@@ -42,6 +79,8 @@ def plot(
             start_x = min_x - 0.1 * (max_x - min_x)
             end_x = max_x + 0.1 * (max_x - min_x)
             test_x = torch.linspace(start_x, end_x, n_test)
+            if torch.cuda.is_available():
+                test_x = test_x.cuda()
             test_classifier_ids = torch.zeros(n_test, dtype=torch.long)
             batch_size = 10
             batches = [test_x[i:i+batch_size] for i in range(0, len(test_x), batch_size)]
@@ -57,27 +96,35 @@ def plot(
                     lower = torch.cat([lower, batch_lower], dim=0)
                     upper = torch.cat([upper, batch_upper], dim=0)
             # Plot predictive means as blue line
+            if torch.cuda.is_available():
+                test_x = test_x.cpu()
+                loc = loc.cpu()
+                lower = lower.cpu()
+                upper = upper.cpu()
             ax.plot(test_x.numpy(), loc.numpy(), 'b')
             # Shade between the lower and upper confidence bounds
             ax.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
         ax.set_ylim([-3, 3])
-        ax.legend(['Observed Data', 'Mean', 'Confidence'])
+        if plot_legend:
+            ax.legend(['Observed Data', 'Mean', 'Confidence'])
+        ax.set_title(opinion_name)
 
 def main():
     this_dir_path = os.path.dirname(os.path.realpath(__file__))
     root_dir_path = os.path.join(this_dir_path, "..", "..")
 
     data_source = "reddit"
-    source_dir = "1sub_1year"
+    source_dir = "4sub_1year"
     if source_dir == "4sub_1year":
         topics_dir_path = os.path.join(root_dir_path, "data", data_source, source_dir)
     elif source_dir == "1sub_1year":
         topics_dir_path = os.path.join(root_dir_path, "data", data_source, source_dir, "topics_minilm_0_2")
     subsample_users = None
-    dataset = RedditOpinionTimelineDataset(topics_dir_path, subsample_users=subsample_users)
+    dataset = RedditOpinionTimelineDataset(topics_dir_path, subsample_users=subsample_users, seed=0)
 
     all_classifier_profiles = dataset.all_classifier_profiles
     stance_targets = dataset.stance_columns
+    stance_names = ['Vaccine Mandates', 'Renter Protections', 'NDP', 'Liberals', 'Conservatives', 'Gun Control', 'Drug Decriminalization', 'Liberal Immigration Policy', 'Canadian Aid to Ukraine']
     X_norm, X, y, classifier_ids = prep_gp_data(dataset)
 
     model_type = 'spline'
@@ -91,30 +138,43 @@ def main():
         lengthscale_loc = month_span_ratio * 0.5
         lengthscale_scale = month_span_ratio * 0.1
 
-        model_list, likelihood_list, model_map, losses = train_gaussian_process(X_norm, y, classifier_ids, stance_targets, all_classifier_profiles, lengthscale_loc=lengthscale_loc, lengthscale_scale=lengthscale_scale, gp_type='ordinal')
-        timestamps, means, confidence_region = get_gp_means(dataset, model_list, likelihood_list, model_map, X_norm, X, y)
+        models, likelihood_list, model_map, losses = train_gaussian_process(X_norm, y, classifier_ids, stance_targets, all_classifier_profiles, lengthscale_loc=lengthscale_loc, lengthscale_scale=lengthscale_scale, gp_type='ordinal')
+        timestamps, means, confidence_region = get_gp_means(dataset, models, likelihood_list, model_map, X_norm, X, y)
     elif model_type == 'spline':
-        model_list, model_map = get_splines(X_norm, y, alpha=1e-1, n_knots=4)
-        timestamps, means = get_spline_means(dataset, model_list, model_map, X_norm, X, y)
+        models, coef_bootstraps, spline_transformers, model_map = get_splines(X_norm, y, alpha=2e-1, n_knots=4)
+        timestamps, means, confidence_region = get_spline_means(dataset, models, coef_bootstraps, spline_transformers, model_map, X_norm, X, y)
 
-    np.save(os.path.join(root_dir_path, "data", data_source, source_dir, "timestamps.npy"), timestamps)
-    np.save(os.path.join(root_dir_path, "data", data_source, source_dir, "means.npy"), means)
-    if model_type == 'gp':
-        np.save(os.path.join(root_dir_path, "data", data_source, source_dir, "confidence_region.npy"), confidence_region)
+    os.makedirs(os.path.join(root_dir_path, "data", data_source, source_dir, model_type), exist_ok=True)
+    np.save(os.path.join(root_dir_path, "data", data_source, source_dir, model_type, "timestamps.npy"), timestamps)
+    np.save(os.path.join(root_dir_path, "data", data_source, source_dir, model_type, "means.npy"), means)
+    np.save(os.path.join(root_dir_path, "data", data_source, source_dir, model_type, "confidence_region.npy"), confidence_region)
 
-    os.makedirs(os.path.join(root_dir_path, "figs", data_source, source_dir, "model_fits"), exist_ok=True)
-    for idx in tqdm.tqdm(range(min(len(model_list), 100)), desc="Plotting model fits"):
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 3))
-        i, k = model_map[idx]
-        model = model_list[idx]
-        train_x = X_norm[i, ~torch.isnan(y[i,:,k])]
-        train_y = y[i, ~torch.isnan(y[i,:,k]), k]
-        if model_type == 'gp':
-            likelihood = likelihood_list[idx]
-            plot(model, likelihood, train_x=train_x, train_y=train_y, plot_observed_data=True, plot_predictions=True, ax=ax)
-        elif model_type == 'spline':
-            plot_spline(model, train_x=train_x, train_y=train_y, plot_observed_data=True, plot_predictions=True, ax=ax)
-        fig.savefig(os.path.join(root_dir_path, "figs", data_source, source_dir, "model_fits", f"{i}_{k}_gp.png"))
+    os.makedirs(os.path.join(root_dir_path, "figs", data_source, source_dir, model_type, "model_fits"), exist_ok=True)
+    user_is = sorted(list(set([i for i, k in model_map])))
+    if len(user_is) > 100:
+        user_is = user_is[:100]
+    for i in tqdm.tqdm(user_is, desc="Plotting model fits"):
+        user_idxs = [idx for idx in range(len(model_map)) if model_map[idx][0] == i]
+        user_ks = [model_map[idx][1] for idx in user_idxs]
+        fig, axes = plt.subplots(nrows=len(user_idxs), ncols=1, figsize=(8, 3*len(user_idxs)))
+        for idx, (model_idx, k) in enumerate(zip(user_idxs, user_ks)):
+            if len(user_idxs) == 1:
+                ax = axes
+            else:
+                ax = axes[idx]
+            opinion_name = stance_names[k]
+            model = models[model_idx]
+            train_x = X[i, ~torch.isnan(y[i,:,k])]
+            train_y = y[i, ~torch.isnan(y[i,:,k]), k]
+            if model_type == 'gp':
+                likelihood = likelihood_list[model_idx]
+                plot(model, likelihood, train_x=train_x, train_y=train_y, plot_observed_data=True, plot_predictions=True, ax=ax, opinion_name=opinion_name, plot_legend=idx==0)
+            elif model_type == 'spline':
+                timeline, mean, confidence_interval = timestamps[i,:,k], means[i,:,k], confidence_region[i,:,k]
+                plot_spline(timeline, mean, confidence_interval, train_x=train_x, train_y=train_y, plot_observed_data=True, plot_predictions=True, ax=ax, opinion_name=opinion_name, plot_legend=idx==0)
+        # fig.autofmt_xdate()
+        fig.tight_layout()
+        fig.savefig(os.path.join(root_dir_path, "figs", data_source, source_dir, model_type, "model_fits", f"{i}_gp.png"))
         plt.close(fig)
 
 
